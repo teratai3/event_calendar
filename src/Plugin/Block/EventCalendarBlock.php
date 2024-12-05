aff1fa6ac190ab5797a244874c55757d5815b93f
terao <>
2024-12-05 16:59:22 +0900

    ブロック追加

---
+++ src/Plugin/Block/EventCalendarBlock.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Drupal\event_calendar\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an Event Calendar Block.
+ *
+ * @Block(
+ *   id = "event_calendar_block",
+ *   admin_label = @Translation("イベントカレンダー"),
+ * )
+ */
+class EventCalendarBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The Database Connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The Entity Type Manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $database, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->database = $database;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $node_types = $this->entityTypeManager->getStorage('node_type')->loadMultiple();
+    $options = [];
+
+    foreach ($node_types as $type_id => $type) {
+      $options[$type_id] = $type->label();
+    }
+
+    $config = $this->getConfiguration();
+    $form['event_node_type'] = [
+      '#type' => 'select',
+      '#title' => 'カレンダーの対象ノードタイプ',
+      '#description' => 'このカレンダー ブロックのノードタイプを選択します。',
+      '#options' => $options,
+      '#default_value' => $config['event_node_type'] ?? '',
+      '#empty_option' => $this->t('- Select -'),
+      '#required' => TRUE,
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    // ユーザー入力値を設定に保存.
+    $this->setConfigurationValue('event_node_type', $form_state->getValue('event_node_type'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $now = new DrupalDateTime('now');
+    $firstDayOfMonth = $now->format('Y-m-01');
+    $lastDayOfMonth = $now->format('Y-m-t');
+
+    $startDate = new DrupalDateTime($firstDayOfMonth);
+    $endDate = new DrupalDateTime($lastDayOfMonth);
+
+    // 現在の月のカレンダーを構築.
+    $calendarDays = [];
+    // 月の最初の曜日 (0=日曜日, 6=土曜日)
+    $startWeekday = (int) $startDate->format('w');
+    // 月の日数.
+    $totalDays = (int) $endDate->format('j');
+
+    // 空白の日付を挿入（前月のプレースホルダー）.
+    for ($i = 1; $i < $startWeekday; $i++) {
+      $calendarDays[] = [
+        'day' => NULL,
+        'has_event' => FALSE,
+      ];
+    }
+
+    // イベントデータを取得.
+    $query = $this->database->select('event_calendars', 'ec');
+    $query->fields('ec', ['start_date', 'end_date', 'nid']);
+    $query->join('node_field_data', 'nfd', 'ec.nid = nfd.nid');
+    $query->fields('nfd', ['type']);
+    $query->condition('nfd.type', 'article', '=');
+    $query->condition('ec.start_date', $lastDayOfMonth, '<=');
+    $query->condition('ec.end_date', $firstDayOfMonth, '>=');
+    $results = $query->execute();
+
+    // イベント期間をマッピング.
+    $eventDates = [];
+
+    foreach ($results as $record) {
+      $eventStart = new DrupalDateTime($record->start_date);
+      $eventEnd = new DrupalDateTime($record->end_date);
+
+      // イベント期間内の日付をすべて取得.
+      $current = clone $eventStart;
+      while ($current <= $eventEnd) {
+        $day = $current->format('j');
+        $eventDates[$day] = TRUE;
+        $current->modify('+1 day');
+      }
+    }
+
+    // 現在月の日付を追加.
+    for ($day = 1; $day <= $totalDays; $day++) {
+      // $calendarDays[] = $day;
+      $calendarDays[] = [
+        'day' => $day,
+        'has_event' => !empty($eventDates[$day]),
+      ];
+    }
+
+    return [
+      '#theme' => 'event_calendar_block',
+      '#calendar_days' => $calendarDays,
+      '#month' => $now->format('m'),
+      '#day' => $now->format('j'),
+      '#year' => $now->format('Y'),
+      '#attached' => [
+        'library' => [
+          'event_calendar/event_calendar_js',
+        ],
+      ],
+    ];
+  }
+
+}
